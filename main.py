a1d112a6440975a1ff5844f62d88c27f94c013ab
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2025-04-19 22:11:54 -0400

    added read table fn, user input

--- main.py
+++ main.py
@@ -2,33 +2,47 @@
 import resources
 import os
 import time
-# from LCD import LCD
+
 
 def main():
-    dbName="contacts"
-    tblName="names"
-    field1="first"
-    field2="last"
-    keepDb=1
+    dbName = "contacts"
+    tblName = "names"
+    field1 = "first"
+    field2 = "last"
+    keepDb = 1
+
+    # Initalize LCD Display
+    # lcd = resources.LCD(2, 0x27, True)
+
+    # User Input
+    fname=input("first: ")
+    lname=input("last: ")
     
-    lcd=resources.LCD(2,0x27,True)
-# Use Try / Catch while using file usage.
+    if fname=="" & lname=="":
+	record1 = ("Boo", "Hoo")
+    else:
+        record1=(fname,lname)
+
+    # Use Try / Catch while using file usage.
+
+    #  Create Database
+    # resources.Database.create(dbName, tblName, field1, field2, keepDb)
+
+    # Add / Insert Record
     
-    
-    resources.Database.create(dbName,tblName,field1,field2,keepDb)
-    
-    record1=("Boo","Hoo")
-    resources.Database.add_record(dbName,tblName,field1,field2,record1)
-    lcd.message(" record recorded")
-    time.sleep(10)
-    lcd.clear()
+      resources.Database.add_record(dbName, tblName, field1, field2, record1)
+
+    # Read Database
+    resources.Database.read_record(dbName, tblName)
+
+    # Output to LCD Display
+    #    lcd.message(" record recorded")
+    #    time.sleep(10)
+    #    lcd.clear()
+
+    # End of Line
     print("end of line")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-
-
-
-
